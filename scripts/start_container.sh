0d25c4da2d09f7b208c638a4a92f0fc651fd58ff
Divyjain0212 <divyj12349@gmail.com>
2026-03-22 11:50:58 +0530

    Updated container port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@
   docker pull divyj0212/divy-online-shop-app:latest
 
   # Run the Docker image as a container
-  docker run -d -p 80:80 divyj0212/divy-online-shop-app:latest
+  docker run -d -p 8080:80 divyj0212/divy-online-shop-app:latest
